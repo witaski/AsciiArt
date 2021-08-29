699135e6f080cd4c3f5cb957c9e493c720dc5c21
Wellington Witaski <wellingtonwitaski@hotmail.com>
2021-08-29 13:08:42 -0300

    Adiciona solução do exercício AsciiArt

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace AsciiArt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+    
+           Console.WriteLine(@" \\()//");
+           Console.WriteLine(@"//(__)\\");
+           Console.WriteLine("||    ||");
         }
     }
 }
